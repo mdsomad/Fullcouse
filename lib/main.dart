f8d2ba60aa998f4e11832d57bb461bca2ffe8254
md somad <mdsomad10@gmail.com>
2022-07-06 11:16:42 +0530

    Dary 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_full_course_project/home_page.dart';
+import 'package:flutter_full_course_project/pages/home_page.dart';
+import 'package:flutter_full_course_project/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-          home: HomePage()
+          themeMode: ThemeMode.light,
+          theme: ThemeData(
+            primarySwatch: Colors.deepPurple
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark
+          ),
+          initialRoute: "/Home",
+          routes: {
+            "/":(context) => LoginPage(),
+            "/Home":(context)=>HomePage(),
+            '/login':(context) => LoginPage()
+          },
     );
   }
 }
