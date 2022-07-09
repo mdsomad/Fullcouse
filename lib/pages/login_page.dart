5416f98340ea999ee667b4a266c0b5b7ee658e52
Md Somad <mdsomad10@gmail.com>
2022-07-09 16:41:04 +0530

    Day 9 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,94 +12,120 @@ class LoginPage  extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
    String name = "";
    bool changeButtan = false;
+   final _fomeKey = GlobalKey<FormState>();
+   
+
+   moveTohome(BuildContext context)async{
+    if(_fomeKey.currentState!.validate()){
+      setState(() {
+         changeButtan =true;
+     }); 
+     await Future.delayed(Duration(seconds: 1)) ;
+     await Navigator.pushNamed(context,MyRoutes.homeRoute);
+     setState(() {
+       changeButtan =false;
+     });
+    }
+   }
   
   
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child:Column(
-        children: [
-          Image.asset("assets/images/login_image.png",
-          fit: BoxFit.cover,),
-          SizedBox(
-            height: 20,
-          ),
-          Text("Welcome $name",
-          style: TextStyle(
-            fontSize: 20,fontWeight: 
-            FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-           Padding(
-             padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-             child: SingleChildScrollView(
-               child: Column(
-                children: [
-                   TextFormField(
-                  decoration:InputDecoration(
-                    hintText: "Enter username",
-                    labelText: "Username"
-                  ),
-                  onChanged: (value){
-                    setState(() {
-                      name = value;
-                    });
-                  },
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration:InputDecoration(
-                    hintText: "Enter password",
-                    labelText: "password"
-                  ),
+      child:Form(
+        key: _fomeKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/images/login_image.png",
+              fit: BoxFit.cover,),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Welcome $name",
+              style: TextStyle(
+                fontSize: 20,fontWeight: 
+                FontWeight.bold),
                 ),
                 SizedBox(
                   height: 20,
                 ),
-                  InkWell(
-                    onTap: (() async{
-                      setState(() {
-                        changeButtan =true;
-                      }); 
-                      await Future.delayed(Duration(seconds: 1)) ;
-                      Navigator.pushNamed(context,MyRoutes.homeRoute); 
-                    }),
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      width:changeButtan?50: 150,
-                      height: 50,
-                      alignment: Alignment.center,
-                      child:changeButtan?Icon(Icons.done,color: Colors.pink,):Text("Login",style: TextStyle(color: Colors.
-                      white,fontWeight: FontWeight.bold,
-                      fontSize: 18
-                      ),
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        // shape:changeButtan?BoxShape.circle:BoxShape.rectangle 
-                        borderRadius: BorderRadius.circular(changeButtan?50:8)
-                      ),
+               Padding(
+                 padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+                 child: Column(
+                  children: [
+                     TextFormField(
+                    decoration:InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username"
                     ),
-                  )
-
-
-                
-                // ElevatedButton(onPressed: (){
-                //   Navigator.pushNamed(context,MyRoutes.homeRoute);            },
-                //  child: Text("Login"),
-                //   style: ElevatedButton.styleFrom(
-                //     primary: Colors.red,minimumSize: Size(120, 50),
-          
-                //   ),
-                // )
-                ],
-               ),
-             ),
-           )
-        ],
+                    validator: (value){
+                        if(value!.isEmpty){
+                          return "username cannot be emoty";
+                        }
+                    },
+                    onChanged: (value){
+                      setState(() {
+                        name = value;
+                      });
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration:InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "password"
+                    ),
+                    validator: (value){
+                       if(value!.isEmpty){
+                        return "passwoed cannot be emoty";
+                       }
+                       else if(value.length<6){
+                         return 'password length should be atlleast 6';
+                       }
+                    },
+                    
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                    Material(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(changeButtan?50:8),
+                      child: InkWell(
+                        onTap: (()=>moveTohome(context)),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width:changeButtan?50: 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child:changeButtan?Icon(Icons.done,color: Colors.pink,):Text("Login",style: TextStyle(color: Colors.
+                          white,fontWeight: FontWeight.bold,
+                          fontSize: 18
+                          ),
+                          ),
+                          
+                        ),
+                      ),
+                    )
+        
+        
+                  
+                  // ElevatedButton(onPressed: (){
+                  //   Navigator.pushNamed(context,MyRoutes.homeRoute);            },
+                  //  child: Text("Login"),
+                  //   style: ElevatedButton.styleFrom(
+                  //     primary: Colors.red,minimumSize: Size(120, 50),
+              
+                  //   ),
+                  // )
+                  ],
+                 ),
+               )
+            ],
+          ),
+        ),
       )
     );
   }
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+
+class MyDrawer extends StatelessWidget {
+  // final url = "https://www.whatsappimages.in/dp-for-girls-images/";
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.pink
+              ),
+              margin: EdgeInsets.zero,
+              accountName: Text("Md Soamd"), accountEmail:Text("mdsomad10@gmail.com"),
+            currentAccountPicture: CircleAvatar(backgroundImage:AssetImage("assets/images/Girl-DP.jpg"),),
+            ),
+          ),
+          ListTile(
+            leading: Icon(CupertinoIcons.home,color: Colors.teal,),
+            title: Text("Home"),
+          ),
+          ListTile(
+            leading: Icon(CupertinoIcons.person,color: Colors.teal,),
+            title: Text("persion"),
+          ),
+          ListTile(
+            leading: Icon(CupertinoIcons.mail,color: Colors.teal,),
+            title: Text("Mall"),
+          ),
+        ],
+      ),
+    );
+  }
+}
