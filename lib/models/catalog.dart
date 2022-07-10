b96d88db33fcb3fa4ad7b939fcdf940ab7fd49e1
Md Somad <mdsomad10@gmail.com>
2022-07-10 11:08:11 +0530

    class add

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final num  price;
+  final String  color;
+  final String image;
+
+  item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+// final peoducts = [
+//     item(id:"Codepur001",
+//      name:"Iphon 13 pro", 
+//      desc:"Apple iphon 13th generation", 
+//      price:10000,
+//       color:"#33505a",
+//        image:"https://www.amazon.in/Apple-iPhone-13-128GB-Product/dp/B09G99CW2N"),
+// ];
